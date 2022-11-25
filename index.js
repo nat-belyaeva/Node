d7cee88de2f84157025aad8c5ef5a4527a55cda5
nat-belyaeva <seonat19@gmail.com>
2022-11-25 22:11:00 +0300

    second commit create modules

--- index.js
+++ index.js
@@ -7,16 +7,16 @@
 // })
 
 //========================== Работа с модулями =====================
-// const os = require('os'); // подключение встроенного модуля
+//const os = require('os'); // подключение встроенного модуля
 // let res = os.platform();
 // console.log(res);
 //========================== Создание своего модуля =====================
 //Это создание либо отдельной папки либо отдельного файла >> далее подключение этого файла внутри др файлов..это и есть
 // создание модуля
 
-const my_math = require('./my_math');
-
-let res1 = my_math.add(4, 5);
-console.log(res1)
-
+// const my_math = require('./my_math');
+import {add, minus}   from './my_math.js'
+let res1 = add(4, 5);
+let res2 = minus(15, 5);
+console.log(`Result: res1 is ${res1} and res2 is ${res2}`);
 
